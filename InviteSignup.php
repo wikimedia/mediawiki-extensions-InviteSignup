bbaee86695cba854077a0f2c4283f4762f12f1c6
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-07-31 15:55:15 +0200

    Breaking change: convert to extension registration
    Requires MediaWiki 1.25 or later
    
    Change-Id: I5bb4ad86b1011e6571c4c15d46c4d93b2b672648

--- InviteSignup.php
+++ InviteSignup.php
@@ -1,44 +1,14 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) die();
-/**
- * An extension that allows users to invite new users to signup to a closed wiki.
- *
- * @file
- * @ingroup Extensions
- *
- * @author Niklas Laxström
- * @copyright Copyright © 2012-2013 Lost in Translations Inc.
- * @license GPL-2.0+
- */
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'InviteSignup',
-	'version' => '2015-07-31',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:InviteSignup',
-	'author' => array( 'Niklas Laxström' ),
-	'descriptionmsg' => 'is-desc',
-);
-
-$dir = __DIR__;
-$wgAutoloadClasses['InviteSignupHooks'] = "$dir/InviteSignupHooks.php";
-$wgAutoloadClasses['InviteStore'] = "$dir/InviteStore.php";
-$wgAutoloadClasses['SpecialInviteSignup'] = "$dir/SpecialInviteSignup.php";
-$wgMessagesDirs['InviteSignup'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['InviteSignup'] = "$dir/InviteSignup.i18n.php";
-$wgExtensionMessagesFiles['InviteSignupAlias'] = "$dir/InviteSignup.alias.php";
-$wgSpecialPages['InviteSignup'] = 'SpecialInviteSignup';
-$wgAvailableRights[] = 'invitesignup';
-
-$wgInviteSignupHash = null;
-
-/**
- * List of groups the invitee will be promoted automatically.
- */
-$wgISGroups = array();
-
-$wgHooks['BeforeInitialize'][] = 'InviteSignupHooks::onBeforeInitialize';
-$wgHooks['UserGetRights'][] = 'InviteSignupHooks::onUserGetRights';
-$wgHooks['UserCreateForm'][] = 'InviteSignupHooks::onUserCreateForm';
-$wgHooks['AddNewAccount'][] = 'InviteSignupHooks::onAddNewAccount';
-$wgHooks['LoadExtensionSchemaUpdates'][] = 'InviteSignupHooks::onLoadExtensionSchemaUpdates';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'InviteSignup' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['InviteSignup'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['InviteSignup'] = __DIR__ . '/InviteSignup.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for InviteSignup extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the InviteSignup extension requires MediaWiki 1.25+' );
+}
--- SpecialInviteSignup.php
+++ SpecialInviteSignup.php
@@ -20,15 +20,6 @@ class SpecialInviteSignup extends SpecialPage {
 		$this->groups = $wgISGroups;
 	}
 
-	// BC for MW <= 1.22
-	public function getPageTitle( $subpage = false ) {
-		if ( method_exists( 'SpecialPage', 'getPageTitle' ) ) {
-			return parent::getPageTitle( $subpage );
-		} else {
-			return parent::getTitle( $subpage );
-		}
-	}
-
 	public function setStore( InviteStore $store ) {
 		$this->store = $store;
 	}
