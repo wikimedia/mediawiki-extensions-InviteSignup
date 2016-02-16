354576447ce5bc004161d8aab11f2d0b01d30eae
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-16 15:29:16 +0100

    Update linters and checkers
    There is no JavaScript in this repo yet apart from
    Gruntfile.js. But seems easier to keep these files
    more consistent across repos. And we might add JS
    files in future.
    
    Change-Id: Idccf0fae10003e65574243ab105fd183c2aecd20

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,36 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
+	'use strict';
+
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
 		jshint: {
+			options: {
+				jshintrc: true
+			},
 			all: [
-				'*.js'
+				'**/*.js',
+				'!node_modules/**'
 			]
 		},
-		banana: {
-			all: 'i18n/'
+		jscs: {
+			src: '<%= jshint.all %>'
 		},
 		jsonlint: {
 			all: [
 				'**/*.json',
 				'!node_modules/**'
 			]
+		},
+		banana: {
+			all: 'i18n/'
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
