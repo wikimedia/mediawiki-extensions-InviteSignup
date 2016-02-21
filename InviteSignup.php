79c583ab68c8eb415ae2cf7c08ea3f6caf91a9d6
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-21 09:55:53 +0100

    Use unique id for alias file
    Change-Id: I35c0616f4b660da1251f90595184ffb0d09a8860

--- InviteSignup.php
+++ InviteSignup.php
@@ -3,7 +3,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'InviteSignup' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['InviteSignup'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['InviteSignup'] = __DIR__ . '/InviteSignup.alias.php';
+	$wgExtensionMessagesFiles['InviteSignupAlias'] = __DIR__ . '/InviteSignup.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for InviteSignup extension. ' .
 		'Please use wfLoadExtension instead, see ' .
